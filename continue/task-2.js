1ab62f7ee47b1b9923cf291e3a226b8909cc2a1b
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-03 15:39:46 +0600

    task-2 added

---
+++ continue/task-2.js
@@ -0,0 +1,29 @@
+// for---------------
+for (let i = 55; i <= 85; i++) {
+    if (i % 2 === 0) {
+        continue;   // Skip even numbers
+    }
+    if (i % 5 === 0) {
+        continue;   // Skip numbers divisible by 5
+    }
+    console.log('for:', i);
+}
+
+
+
+
+// while loop---------------
+let i = 55;
+
+while (i <= 85) {
+    if (i % 2 === 0) {
+        i++;
+        continue;   // Skip even numbers
+    }
+    if (i % 5 === 0) {
+        i++;
+        continue;   // Skip numbers divisible by 5
+    }
+    console.log('while:', i);
+    i++;
+}
